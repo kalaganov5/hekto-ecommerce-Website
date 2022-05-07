4d0e642a54555c40a5a778af2e020f21b7293822
kalaganov5 <kalaganov5@gmail.com>
2022-05-07 13:24:48 +0300

    refactoring slider header

--- source/js/slider-header.js
+++ source/js/slider-header.js
@@ -1,6 +1,10 @@
 import Swiper from 'https://unpkg.com/swiper@7/swiper-bundle.esm.browser.min.js';
 
 const sliderHeader = new Swiper(".slider", {
+  effect: 'flip',
+  flipEffect: {
+    slideShadows: false,
+  },
   pagination: {
     el: ".navigation-slider",
           clickable: true,
@@ -8,6 +12,11 @@ const sliderHeader = new Swiper(".slider", {
               return `<span class="navigation-slider__label ${className}"></span>`;
           },
   },
+  keyboard: {
+    enabled: true,
+    onlyInViewport: false,
+  },
+  grabCursor: true,
 });
 
 export {sliderHeader};
